c24fa203e1dccf82a6ebc6953455f87551e60263
edvno177 <nordin.edvin@gmail.com>
2022-02-27 18:27:24 +0100

    Images on hover and clean up

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import pandas as pd
-from bokeh.layouts import layout
-from bokeh.models import HoverTool, ColumnDataSource
+import json
+from PIL import Image
+import os
+
+from bokeh.embed import json_item
+from bokeh.layouts import layout, gridplot
+from bokeh.models import HoverTool, ColumnDataSource, CustomJSHover, CategoricalColorMapper
 from bokeh.plotting import figure, show
 from bokeh.models.widgets import Panel, Tabs
 
-dataset = pd.read_csv("pokedex_(Update_05.20).csv")
-datasetCDS = ColumnDataSource(data=dataset)
-total = dataset["hp"] + dataset["attack"] + dataset["sp_attack"] + dataset["defense"] + dataset["sp_defense"] + dataset[
+dataframe = pd.read_csv("pokedex_(Update_05.20).csv")
+datasetCDS = ColumnDataSource(data=dataframe)
+total = dataframe["hp"] + dataframe["attack"] + dataframe["sp_attack"] + dataframe["defense"] + dataframe["sp_defense"] + dataframe[
     "speed"]
 
 #adding total as a new column in datasetCDS
-datasetCDS.data["total"]=total
+datasetCDS.data["total"] = total
 
-zsrc = ColumnDataSource(data={'image':[z]})
+dataframe['formPN'] = dataframe['pokedex_number'].apply(lambda x: '{0:0>3}'.format(x))
+#creates the url to the images
+dataframe['url'] = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/' + dataframe['formPN'].astype(str) + '.png'
+dataframe['url'] = dataframe['url'].str.replace(' ', '-')
+dataframe['url'] = dataframe['url'].str.lower()
+
+datasetCDS.data["url"] = dataframe['url']
+#dataframe.to_excel("test1.xlsx")
+
+
+
+
 """hp = dataset["hp"]
 attack = dataset["attack"]
 defense = dataset["defense"]
@@ -24,7 +40,21 @@ total = dataset["hp"] + dataset["attack"] + dataset["sp_attack"] + dataset["defe
 dex = dataset["pokedex_number"]"""
 
 
-TOOLTIPS = [
+TOOLTIPS = """
+    <div>
+        <div>
+            <img src="@url" height="142" alt="@name" width="142"
+                style="float: left; margin: 0px 15px 15px 0px;"
+                border="1">
+            </img>
+        </div>
+        <div>
+            <span style="font-size: 17px; font-weight: bold;">@pokedex_number</span>
+            <span style="font-size: 15px; color: #966;">@name</span>
+        </div>
+    </div>
+"""
+"""[
     ("Pokedex", "@pokedex_number"),
     ("hp", "@hp"),
     ("attack", "@attack"),
@@ -32,36 +62,50 @@ TOOLTIPS = [
     ("defense", "@defense"),
     ("sp_defense", "@sp_defense"),
     ("speed", "@speed"),
-]
+    ("total", "@total"),
+]"""
+
+color_mapper = CategoricalColorMapper(palette=["#EAA3DC", "#B8B8D0", "#765747", "#7636F6", "#745797", "#BF9F38", "#A5B82B",
+                                               "#FF4980", "#AC8FEF", "#E9C26E", "#AF399D", "#D51C0D", "#7FDADA", "#55CA59",
+                                               "#FFCE2E", "#5591F0", "#FF7919", "#A9A879"],
+                                      factors=["Fairy", "Steel", "Dark", "Dragon", "Ghost", "Rock", "Bug", "Psychic",
+                                               "Flying", "Ground", "Poison", "Fight", "Ice", "Grass", "Electric",
+                                               "Water", "Fire", "Normal"])
 
 
-php = figure(x_axis_label="total", y_axis_label="hp", tooltips=TOOLTIPS)
-php.circle(x="total", y="hp", source=datasetCDS, color="black", size=3)
+php = figure(x_axis_label="total", y_axis_label="hp", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+php.circle(x="total", y="hp", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab1 = Panel(child=php, title="hp")
 
-patt = figure(x_axis_label="total", y_axis_label="attack", tooltips=TOOLTIPS)
-patt.circle(x="total", y="attack", source=datasetCDS, color="black", size=3)
+patt = figure(x_axis_label="total", y_axis_label="attack", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+patt.circle(x="total", y="attack", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab2 = Panel(child=patt, title="attack")
 
-pspa = figure(x_axis_label="total", y_axis_label="sp_attack", tooltips=TOOLTIPS)
-pspa.circle(x="total", y="sp_attack", source=datasetCDS, color="black", size=3)
+pspa = figure(x_axis_label="total", y_axis_label="sp_attack", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+pspa.circle(x="total", y="sp_attack", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab3 = Panel(child=pspa, title="sp_attack")
 
-pdef = figure(x_axis_label="total", y_axis_label="defense", tooltips=TOOLTIPS)
-pdef.circle(x="total", y="defense", source=datasetCDS, color="black", size=3)
+pdef = figure(x_axis_label="total", y_axis_label="defense", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+pdef.circle(x="total", y="defense", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab4 = Panel(child=pdef, title="defense")
 
-pspd = figure(x_axis_label="total", y_axis_label="sp_defense", tooltips=TOOLTIPS)
-pspd.circle(x="total", y="sp_defense", source=datasetCDS, color="black", size=3)
+pspd = figure(x_axis_label="total", y_axis_label="sp_defense", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+pspd.circle(x="total", y="sp_defense", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab5 = Panel(child=pspd, title="sp_defense")
 
-pspe = figure(x_axis_label="total", y_axis_label="speed", tooltips=TOOLTIPS)
-pspe.circle(x="total", y="speed", source=datasetCDS, color="black", size=3)
+pspe = figure(x_axis_label="total", y_axis_label="speed", tooltips=TOOLTIPS, active_scroll="wheel_zoom")
+pspe.circle(x="total", y="speed", source=datasetCDS, color={'field': 'type_1', 'transform': color_mapper}, size=5)
 tab6 = Panel(child=pspe, title="speed")
 
 tabs = Tabs(tabs=[tab1, tab2, tab3, tab4, tab5, tab6])
+
+#bars = figure(x_axis_label="total", y_axis_label="attack", tooltips=TOOLTIPS)
+#bars.vbar(x="total", top="attack", source=datasetCDS, color="black", bottom=0, width=1)
+
+p=gridplot([[tabs]], toolbar_location=None)
+
 # show result
-show(tabs)
+show(p)
 
 """
 ''' An interactivate categorized chart based on a movie dataset.
